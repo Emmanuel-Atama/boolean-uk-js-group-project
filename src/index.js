5b92df96c34299122de773a3b2afca027197b346
Emmanuel Atama <emmanuelatama@outlook.com>
2021-08-11 12:15:41 +0100

    Recent Update

--- src/index.js
+++ src/index.js
@@ -8,65 +8,63 @@ let stateData = {
 };
 
 function fetchListSection() {
-  const url = "/db/db.json";
+  const url = "http://localhost:3000/expeditions";
   fetch(url)
     .then((res) => res.json())
-    .then((data) => {
-      console.log("Inside GET Fetch: ", data);
-      stateData = {
-        ...stateData,
-        expedition: data
-      };
-      renderHeaderSection(data);
+    .then((expeditions) => {
+      console.log("Inside GET Fetch Data: ", expeditions);
+      // stateData = {
+      //   ...stateData,
+      //   expedition: data
+      // };
+      renderHeaderSection(expeditions);
     });
-}
+} 
 fetchListSection();
 
-function fetchImageOfTheDay() {
-  const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-  fetch(url)
-    .then((res) => res.json())
-    .then((imageData) => {
-      // console.log("Inside GET Fetch: ", imageData);
-      stateData = {
-        ...stateData,
-        imageOfTheDay: imageData
-      };
-    });
-}
-fetchImageOfTheDay();
+// function fetchImageOfTheDay() {
+//   const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((imageData) => {
+//       console.log("Inside GET Fetch Image: ", imageData);
+//       // stateData = {
+//       //   ...stateData,
+//       //   imageOfTheDay: imageData
+//       // };
+//     });
+// }
+// fetchImageOfTheDay();
 
 function renderHeaderSection(heads) {
   const bodyEl = document.querySelector("body");
   console.log("Body: ", bodyEl);
 
   const headerEl = document.createElement("header");
-  headerEl.className = "main-header two-column-grid__expand-right";
+  headerEl.className = "main-header grid-auto-columns";
   bodyEl.append(headerEl);
 
-  const headerContainerEl = document.createElement("div");
-  headerContainerEl.className = "two-column-grid";
-  headerEl.append(headerContainerEl);
 
   const headerImageEl = document.createElement("img");
   headerImageEl.src =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE00lXGflW5rgMyvlGeZT_3CBBJ4_4N8btLQ&usqp=CAU";
-  headerContainerEl.append(headerImageEl);
+  headerEl.append(headerImageEl);
 
   const headerTitleEl = document.createElement("h2");
   headerTitleEl.className = "title-section";
   headerTitleEl.innerText = "Welcome To LanEm Space Program";
-  headerContainerEl.append(headerTitleEl);
+  headerEl.append(headerTitleEl);
 
   const navEl = document.createElement("nav");
   headerEl.append(navEl);
 
+  const listElTitle = document.createElement("h3");
+  listElTitle.innerText = "Where Do You Want To Visit?";
+  navEl.append(listElTitle);
+
   const listEl = document.createElement("ul");
-  listEl.className = "grid-auto-columns";
+  listEl.className = "grid-auto-columns text-align";
   navEl.append(listEl);
-  const strongEl = document.createElement("strong");
-  strongEl.innerText = "Where Do You Want To Visit";
-  listEl.append(strongEl);
 
   for (let i = 0; i < heads.length; i++) {
     const head = heads[i];
@@ -78,6 +76,7 @@ function renderHeaderSection(heads) {
     listItemEL.append(linkEl);
   }
 }
+
 
 function renderLeftAsideImage() {}
 renderLeftAsideImage();
