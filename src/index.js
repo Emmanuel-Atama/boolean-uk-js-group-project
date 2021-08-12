654fb12c0061d08a0628f5e30aed32e47455a271
Lana Ferrari <ferrarirangel@gmail.com>
2021-08-12 17:52:42 +0100

    Changes
    Co-authored-by: Emmanuel-Atama <Emmanuel-Atama@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let stateData = {
   expedition: [],
-  imageOfTheDay: {},
   filters: {
     type: "",
-    price: null
+    price: null,
+    number: null
   }
 };
 
@@ -13,17 +13,18 @@ function fetchListSection() {
     .then((res) => res.json())
     .then((expeditions) => {
       console.log("Inside GET Fetch Data: ", expeditions);
-      // stateData = {
-      //   ...stateData,
-      //   expedition: data
-      // };
+      stateData = {
+        ...stateData,
+        expedition: expeditions
+      };
       renderHeaderSection(expeditions);
+      renderListOfExpedition(expeditions);
     });
-} 
+}
 fetchListSection();
 
-const bodyEl = document.querySelector("body")
-console.log("Inside body: ", bodyEl)
+const bodyEl = document.querySelector("body");
+console.log("Inside body: ", bodyEl);
 
 function renderHeaderSection(heads) {
   const headerEl = document.querySelector(".main-header");
@@ -53,6 +54,7 @@ function renderHeaderSection(heads) {
   for (let i = 0; i < heads.length; i++) {
     const head = heads[i];
     const listItemEL = document.createElement("li");
+    listItemEL.className = "header-list";
     listEl.append(listItemEL);
     const linkEl = document.createElement("a");
     linkEl.href = "./index.html";
@@ -61,12 +63,12 @@ function renderHeaderSection(heads) {
   }
 }
 
-function renderLeftAside () {
-  const mainEl = document.querySelector(".main-section")
+function renderLeftAside() {
+  const mainContainerEl = document.querySelector(".left-aside");
 
-  const leftAsideEl = document.createElement("aside")
-  leftAsideEl.className = "left-aside"
-  mainEl.append(leftAsideEl)
+  const leftAsideEl = document.createElement("aside");
+  leftAsideEl.className = "left-aside";
+  mainContainerEl.append(leftAsideEl);
 
   const h2El = document.createElement("h2");
   h2El.innerText = "Filter By";
@@ -104,42 +106,201 @@ function renderLeftAside () {
   optionEl3.value = "price";
   optionEl3.innerText = "Price";
   filterSelectEl.append(optionEl3);
-
 }
-renderLeftAside()
+renderLeftAside();
 
-function renderListOfExpedition() {
-  const mainEl = document.querySelector(".main-section")
-  // console.log("Inside main: ", mainEl)
+function renderListOfExpedition(planets) {
+  const mainContainerEl = document.querySelector(".center-section");
+  // console.log("Inside main: ", mainContainerEl)
 
-const containerEl = document.createElement("div")
-  containerEl.className = "main-section"
-  mainEl.append(containerEl)
-   
-const listEl = document.createElement("ul")
-listEl.className = "responsive-grid"
- containerEl.append(listEl)
+  const mainEl = document.createElement("main");
+  mainEl.className = "center-section";
+  mainContainerEl.append(mainEl);
 
- const listItemEl = document.createElement("li")
- listEl.append(listItemEl)
+  const listEl = document.createElement("ul");
+  // listEl.className = "responsive-grid"
+  mainEl.append(listEl);
+  for (let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    const listItemEl = document.createElement("li");
+    listItemEl.className = "list-space"
 
- const listImageEl = document.createElement("img")
- listEl.append(listImageEl)
+    listEl.append(listItemEl);
 
- const listTitleEl = document.createElement("h3")
- listTitleEl.innerText = location
- listEl.append(listTitleEl)
+    const imageSrc = planet.src;
 
+    const listImageEl = document.createElement("img");
+    listImageEl.setAttribute("width", "100%");
+    listImageEl.setAttribute("height", "400");
+    listImageEl.setAttribute("src", "https://i.ytimg.com/vi/yUbQnHvXYEI/maxresdefault.jpg");
+    listItemEl.append(listImageEl);
 
+    const listItemContainerEl = document.createElement("div");
+    listItemContainerEl.className = "three-column-grid";
+    listItemEl.append(listItemContainerEl);
+
+    const listTitleEl = document.createElement("h3");
+    listTitleEl.innerText = planet.location + " Expedition";
+    listItemContainerEl.append(listTitleEl);
+
+    const listParagraphEl = document.createElement("p");
+    listParagraphEl.innerText = `Price: ${planet.price}`;
+    listItemContainerEl.append(listParagraphEl);
+
+    const listButtonEl = document.createElement("button");
+    listButtonEl.innerText = "Book Now";
+    listButtonEl.setAttribute("width", "40");
+    listButtonEl.setAttribute("height", "40");
+
+    // Add event listener
+    listButtonEl.addEventListener("click", (event) => {
+      event.preventDefault()
+      renderForm();
+
+    })
+
+    listItemContainerEl.append(listButtonEl);
+  }
 }
-renderListOfExpedition();
 
+function renderRightAside() {
+  const mainContainerEl = document.querySelector(".right-aside");
 
-function renderRightAside () {
-  const mainEl = document.querySelector(".main-section")
-
-  const rightAsideEl = document.createElement("aside")
-  rightAsideEl.className = "right-aside"
-  mainEl.append(rightAsideEl)
+  const rightAsideEl = document.createElement("aside");
+  rightAsideEl.className = "right-aside";
+  mainContainerEl.append(rightAsideEl);
 }
-renderRightAside()
+renderRightAside();
+
+
+function renderForm(){
+  const mainContainerEl = document.querySelector(".right-aside");
+
+  const formEl =  document.createElement("form");
+  const labelEl = document.createElement("label");
+  labelEl.innerText = "Pick A Date & Time";
+  const inputEl = document.createElement("input");
+  inputEl.type = "datetime-local";
+  inputEl.id = "meeting-time";
+  inputEl.min = "2021-08-07T00:00";
+  inputEl.max = "2021-12-14T00:00";
+
+const buttonEl = document.createElement("button");
+buttonEl.innerText = "Submit";
+buttonEl.type = "Submit";
+buttonEl.className = "favorite styled";
+
+const selectLabelEl = document.createElement("label")
+selectLabelEl.for = "number-of-travelers";
+
+const selectEl = document.createElement("select")
+selectEl.name =  "number-of-travelers";
+selectEl.id =  "number-of-travelers";
+
+
+// Button Event Listener
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const numberOfTicketsValue = selectEl.value;
+    const dateTimeValue = inputEl.value;
+
+   const eId = stateData.expedition
+
+    const bookingToCreate = {
+      expeditionId: eId.id,
+      dateTime: dateTimeValue,
+      numberOfTickets: numberOfTicketsValue
+    };
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(bookingToCreate)
+    };
+const url = "http://localhost:3000/bookings"
+    fetch(url, fetchOptions)
+      .then((res) => res.json())
+      .then((newBooking) => {
+        console.log("Inside POST Fetch: ", newBooking);
+        stateData = {
+          ...stateData,
+          bookings: newBooking
+        };
+
+        // renderCart(newBooking);
+      });
+
+    renderCart();
+  })
+
+  const defaultOptionEl = document.createElement("option");
+  defaultOptionEl.value = "";
+  defaultOptionEl.innerText = "Select how many tickets you need";
+  selectEl.append(defaultOptionEl);
+
+  const optionEl1 = document.createElement("option");
+  optionEl1.value = "1";
+  optionEl1.innerText = 1;
+
+  const optionEl2 = document.createElement("option");
+  optionEl2.value = "2";
+  optionEl2.innerText = 2;
+
+  const optionEl3 = document.createElement("option");
+  optionEl3.value = "3";
+  optionEl3.innerText = 3;
+
+  const optionEl4 = document.createElement("option");
+  optionEl4.value = "4";
+  optionEl4.innerText = 4;
+
+  selectEl.append(optionEl1, optionEl2, optionEl3, optionEl4)
+
+formEl.append(labelEl, inputEl, selectLabelEl, selectEl, buttonEl);
+mainContainerEl.append(formEl);
+}
+
+
+function renderCart() { 
+const containerDivEl = document.querySelector(".main-section")
+const rightAsideEl = containerDivEl.querySelector(".right-side")
+
+console.log(rightAsideEl);
+
+const cartContainerEl = document.createElement("div")
+cartContainerEl.id = "cart"
+cartContainerEl.className = "cart-styling"
+rightAsideEl.append(cartContainerEl);
+
+const cartContainerTitleEl = document.createElement("h2")
+cartContainerTitleEl.innerText = "My Items"
+
+const price = stateData.expedition.price;
+const dateTime = stateData.bookings.dateTime;
+const numberOfTickets = stateData.bookings.numberOfTickets;
+
+const priceEl =  document.createElement("p");
+priceEl.innerText = `Price per ticket: ${price}`
+cartContainerEl.append(priceEl);
+
+const dateEl = document.createElement("p");
+dateEl.innerText = `Date/Time ${dateTime}`
+cartContainerEl.append(dateEl);
+
+const finalPriceEl = document.createElement("p");
+  finalPriceEl.innerText = "Final price:";
+  //${price * numberOfTickets}
+  cartContainerEl.append(finalPriceEl);
+
+const buttonChangeEl = document.createElement("button");
+buttonChangeEl.innerText = "Change Booking";
+
+const buttonDeleteEl = document.createElement("button");
+buttonDeleteEl.innerText = "Delete Booking";
+
+
+// Change date / Time. (Patch)
+// Cancel purchase(delete)
+}
